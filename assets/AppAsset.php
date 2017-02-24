1664b939e81fb3cb283967a88ab838f177226b86
Alex Yashkin <alex.yashkin@gmail.com>
2017-02-24 21:05:55 +0300

    Added Magnific Popup for large images

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -21,9 +21,11 @@ class AppAsset extends AssetBundle
         'css/site.css',
     ];
     public $js = [
+        'js/main.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
+        'app\assets\MagnificPopupAsset',
     ];
 }
---
+++ web/js/main.js
@@ -0,0 +1,12 @@
+jQuery(function ($) {
+    "use strict";
+
+    $('.image').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        mainClass: 'mfp-img-mobile',
+        image: {
+            verticalFit: true
+        }
+    });
+});
