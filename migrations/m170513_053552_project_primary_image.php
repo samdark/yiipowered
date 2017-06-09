864be188d756cbd42f329d7b98e754ff73cc75d4
prodex <iprodex@mail.ru>
2017-06-09 08:15:00 +0700

    add foreign key

--- migrations/m170513_053552_project_primary_image.php
+++ migrations/m170513_053552_project_primary_image.php
@@ -7,6 +7,7 @@ class m170513_053552_project_primary_image extends Migration
     public function safeUp()
     {
         $this->addColumn('{{%project}}', 'primary_image_id', $this->integer()->null()->comment('Primary image'));
+        $this->addForeignKey('fk-project-primary_image_id', '{{%project}}', 'primary_image_id', '{{%image}}', 'id', 'SET NULL');
         
         $query = "UPDATE project p SET p.primary_image_id = (SELECT i.id FROM image i WHERE i.project_id = p.id ORDER BY i.id LIMIT 1)";
         $this->execute($query);
@@ -14,6 +15,7 @@ class m170513_053552_project_primary_image extends Migration
 
     public function safeDown()
     {
+        $this->dropForeignKey('fk-project-primary_image_id', '{{%project}}');
         $this->dropColumn('{{%project}}', 'primary_image_id');
     }
 }
