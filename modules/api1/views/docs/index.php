92390218214de00543dfaeb7f7aed8540eb5eef8
Alexander Makarov <sam@rmcreative.ru>
2017-07-24 18:50:43 +0300

    Fixed API docs layout

--- modules/api1/views/docs/index.php
+++ modules/api1/views/docs/index.php
@@ -1,2 +1,12 @@
 <?php
-echo $content;
+/** @var string $content */
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+$this->title = 'YiiPowered API 1.0';
+?>
+<div class="site-api-10">
+    <div class="container">
+        <?= $content ?>
+    </div>
+</div>
