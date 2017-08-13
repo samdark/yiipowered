4e1c87b4b85a7aaf5d454318adb9374fe4a1bd2f
prodex <iprodex@mail.ru>
2017-08-13 19:04:46 +0700

    Set published_to_twitter to true for all existing projects.

--- migrations/m170813_061427_add_published_to_twitter.php
+++ migrations/m170813_061427_add_published_to_twitter.php
@@ -7,6 +7,9 @@ class m170813_061427_add_published_to_twitter extends Migration
     public function safeUp()
     {
         $this->addColumn('{{%project}}', 'published_to_twitter', $this->boolean()->defaultValue(false)->notNull());
+        
+        $this->update('{{%project}}', ['published_to_twitter' => true]);
+        
         $this->createIndex('idx-project-status-published_to_twitter', '{{%project}}', ['status', 'published_to_twitter']);
     }
 
