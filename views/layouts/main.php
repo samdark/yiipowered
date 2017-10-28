2e9196dc2494fc0899e21ab000d468055ac9fdc0
Kirill Arutyunov <kirill@arutyunov.me>
2017-10-28 22:08:56 +0300

    Small css fixes: place footer to the bottom of the screen

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -28,111 +28,113 @@ AppAsset::register($this);
 
 <?php $this->beginBody() ?>
     <div class="wrap">
-        <?php
-        NavBar::begin([
-            'brandLabel' => '<span class="yii-logo"></span> YiiPowered',
-            'brandUrl' => Yii::$app->homeUrl,
-            'options' => [
-                'class' => 'navbar-inverse navbar-fixed-top',
-            ],
-        ]);
-
-        $menuItems = [
-            [
-                'label' => Html::tag('span', '<span> ' . Yii::t('app', 'Explore projects'). '</span>', ['class' => 'fa fa-search']),
-                'encode' => false,
-                'url' => ['/projects'],
-                'linkOptions' => ['alt' => Yii::t('app', 'Explore projects'), 'title' => Yii::t('app', 'Explore projects')],
-            ],
-            [
-                'label' => Html::tag('span', '<span> ' . Yii::t('app', 'RSS feed') . '</span>', ['class' => 'fa fa-rss-square']),
-                'encode' => false,
-                'url' => ['/project/rss'],
-                'linkOptions' => [
-                    'alt' => Yii::t('app', 'RSS feed'),
-                    'title' => Yii::t('app', 'RSS feed'),
-                ]
-            ],
-            [
-                'label' => Yii::t('project', 'Top {n}', ['n' => Yii::$app->params['project.maxTopProjects']]),
-                'url' => ['/project/top-projects'],
-            ]
-        ];
-        if (Yii::$app->user->isGuest) {
-            $menuItems[] = [
-                'label' => Html::tag('span', '<span> ' . Yii::t('app', 'Login') . '</span>', ['class' => 'fa fa-sign-in']),
-                'encode' => false,
-                'url' => ['/site/login'],
-                'linkOptions' => ['alt' => Yii::t('app', 'Login'), 'title' => Yii::t('app', 'Login')],
-            ];
-        } else {
-            $menuItems[] = [
-                'label' => Yii::t('user', 'Manage users'),
-                'url' => ['/user/index'],
-                'visible'=> \Yii::$app->user->can('manage_users'),
-            ];
-            $menuItems[] = [
-                'label' => Html::tag('span', '<span> ' . Yii::t('app', 'Bookmarks') . '</span>', ['class' => 'fa fa-bookmark']),
-                'encode' => false,
-                'url' => ['/project/bookmarks'],
-                'linkOptions' => ['alt' => Yii::t('app', 'Bookmarks'), 'title' => Yii::t('app', 'Bookmarks')],
-            ];
-            $menuItems[] = ['label' => Yii::$app->user->identity->username, 'url' => ['/user/view', 'id' => \Yii::$app->user->id]];
-            $menuItems[] = [
-                'label' => Html::tag('span', '<span> ' . Yii::t('app', 'Logout') . '</span>', ['class' => 'fa fa-sign-out']),
-                'url' => ['/site/logout'],
-                'encode' => false,
-                'linkOptions' => [
-                    'data-method' => 'post',
-                    'alt' => Yii::t('app', 'Logout'),
-                    'title' => Yii::t('app', 'Logout'),
+        <div class="content">
+            <?php
+            NavBar::begin([
+                'brandLabel' => '<span class="yii-logo"></span> YiiPowered',
+                'brandUrl' => Yii::$app->homeUrl,
+                'options' => [
+                    'class' => 'navbar-inverse navbar-fixed-top',
                 ],
+            ]);
+
+            $menuItems = [
+                [
+                    'label' => Html::tag('span', '<span> ' . Yii::t('app', 'Explore projects'). '</span>', ['class' => 'fa fa-search']),
+                    'encode' => false,
+                    'url' => ['/projects'],
+                    'linkOptions' => ['alt' => Yii::t('app', 'Explore projects'), 'title' => Yii::t('app', 'Explore projects')],
+                ],
+                [
+                    'label' => Html::tag('span', '<span> ' . Yii::t('app', 'RSS feed') . '</span>', ['class' => 'fa fa-rss-square']),
+                    'encode' => false,
+                    'url' => ['/project/rss'],
+                    'linkOptions' => [
+                        'alt' => Yii::t('app', 'RSS feed'),
+                        'title' => Yii::t('app', 'RSS feed'),
+                    ]
+                ],
+                [
+                    'label' => Yii::t('project', 'Top {n}', ['n' => Yii::$app->params['project.maxTopProjects']]),
+                    'url' => ['/project/top-projects'],
+                ]
             ];
-        }
-        ?>
+            if (Yii::$app->user->isGuest) {
+                $menuItems[] = [
+                    'label' => Html::tag('span', '<span> ' . Yii::t('app', 'Login') . '</span>', ['class' => 'fa fa-sign-in']),
+                    'encode' => false,
+                    'url' => ['/site/login'],
+                    'linkOptions' => ['alt' => Yii::t('app', 'Login'), 'title' => Yii::t('app', 'Login')],
+                ];
+            } else {
+                $menuItems[] = [
+                    'label' => Yii::t('user', 'Manage users'),
+                    'url' => ['/user/index'],
+                    'visible'=> \Yii::$app->user->can('manage_users'),
+                ];
+                $menuItems[] = [
+                    'label' => Html::tag('span', '<span> ' . Yii::t('app', 'Bookmarks') . '</span>', ['class' => 'fa fa-bookmark']),
+                    'encode' => false,
+                    'url' => ['/project/bookmarks'],
+                    'linkOptions' => ['alt' => Yii::t('app', 'Bookmarks'), 'title' => Yii::t('app', 'Bookmarks')],
+                ];
+                $menuItems[] = ['label' => Yii::$app->user->identity->username, 'url' => ['/user/view', 'id' => \Yii::$app->user->id]];
+                $menuItems[] = [
+                    'label' => Html::tag('span', '<span> ' . Yii::t('app', 'Logout') . '</span>', ['class' => 'fa fa-sign-out']),
+                    'url' => ['/site/logout'],
+                    'encode' => false,
+                    'linkOptions' => [
+                        'data-method' => 'post',
+                        'alt' => Yii::t('app', 'Logout'),
+                        'title' => Yii::t('app', 'Logout'),
+                    ],
+                ];
+            }
+            ?>
 
-        <ul class="navbar-nav navbar-right nav">
-            <li class="dropdown">
-                <a href="#" data-toggle="dropdown" class="dropdown-toggle"><span class="fa fa-globe"></span> <b class="caret"></b></a>
-                <?= \app\widgets\LanguageDropdown::widget() ?>
-            </li>
-        </ul>
+            <ul class="navbar-nav navbar-right nav">
+                <li class="dropdown">
+                    <a href="#" data-toggle="dropdown" class="dropdown-toggle"><span class="fa fa-globe"></span> <b class="caret"></b></a>
+                    <?= \app\widgets\LanguageDropdown::widget() ?>
+                </li>
+            </ul>
 
-        <?= Nav::widget([
-            'options' => ['class' => 'navbar-nav navbar-right'],
-            'items' => $menuItems,
-        ]); ?>
+            <?= Nav::widget([
+                'options' => ['class' => 'navbar-nav navbar-right'],
+                'items' => $menuItems,
+            ]); ?>
 
-        <ul class="navbar-nav navbar-right nav">
-            <li>
-                <?= yii\helpers\Html::a(Yii::t('project', 'Add project'), ['project/create'], ['class' => 'btn-add-project']) ?>
-            </li>
-        </ul>
+            <ul class="navbar-nav navbar-right nav">
+                <li>
+                    <?= yii\helpers\Html::a(Yii::t('project', 'Add project'), ['project/create'], ['class' => 'btn-add-project']) ?>
+                </li>
+            </ul>
 
-        <?php NavBar::end(); ?>
+            <?php NavBar::end(); ?>
 
-        <div class="content-wrapper">
-            <?= Breadcrumbs::widget([
-                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-            ]) ?>
-            <?= Alert::widget() ?>
-            <?= $content ?>
+            <div class="content-wrapper">
+                <?= Breadcrumbs::widget([
+                    'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+                ]) ?>
+                <?= Alert::widget() ?>
+                <?= $content ?>
+            </div>
         </div>
+
+        <footer class="footer">
+            <div class="container">
+                <div class="left">
+                    &copy; YiiPowered <?= date('Y') ?> |
+                    <a href="https://twitter.com/yiipowered">Twitter</a> |
+                    <?= Html::a(Yii::t('app', 'About'), ['/site/about']) ?>
+                </div>
+
+                <div class="right">
+                    <?= Yii::powered() ?>
+                </div>
+            </div>
+        </footer>
     </div>
-
-    <footer class="footer">
-        <div class="container">
-            <div class="left">
-                &copy; YiiPowered <?= date('Y') ?> |
-                <a href="https://twitter.com/yiipowered">Twitter</a> |
-                <?= Html::a(Yii::t('app', 'About'), ['/site/about']) ?>
-            </div>
-
-            <div class="right">
-                <?= Yii::powered() ?>
-            </div>
-        </div>
-    </footer>
 
     <?php GoogleAnalytics::track('UA-96041959-1') ?>
 
