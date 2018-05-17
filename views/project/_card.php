678006e695981c13dd6b3d4c039c89fb4c35417f
dungpx <dungphanxuan999@gmail.com>
2018-05-17 08:29:56 +0700

    Fixes #69, fixes #70: Added lazy loading of images and Vietnamese language

--- views/project/_card.php
+++ views/project/_card.php
@@ -8,11 +8,12 @@ use app\widgets\Vote;
 use yii\helpers\Html;
 use yii\helpers\Url;
 
+$bgImg = Yii::$app->request->baseUrl . '/img/project_no_image.png';
 ?>
 
 <article class="<?= $model->getStatusClass() ?>">
     <a class="details" href="<?= Url::to(['project/view', 'id' => $model->id, 'slug' => $model->slug]) ?>">
-        <img class="img-responsive" src="<?= $model->getPrimaryImageThumbnailRelativeUrl() ?>" />
+        <img class="img-responsive" src="<?=$bgImg?>" data-src="<?= $model->getPrimaryImageThumbnailRelativeUrl() ?>" />
         <h1><?= Html::encode($model->title) ?></h1>
     </a>
 
