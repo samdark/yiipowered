62070dc5058acb2d863397ffd968cb6ac1daaea9
Alexander Makarov <sam@rmcreative.ru>
2022-02-16 10:26:42 +0300

    Add badges condition

--- views/project/_card.php
+++ views/project/_card.php
@@ -13,6 +13,7 @@ $bgImg = Yii::$app->request->baseUrl . '/img/project_no_image.png';
 ?>
 
 <article class="<?= $model->getStatusClass() ?>">
+    <?php if ($model->is_featured || $model->verified || $model->check_result === CheckerResult::NOT_YII): ?>
     <div class="badges">
         <?php if ($model->is_featured): ?>
             <span class="featured" aria-hidden="true"></span>
@@ -26,6 +27,7 @@ $bgImg = Yii::$app->request->baseUrl . '/img/project_no_image.png';
             <span class="warning" aria-hidden="true"></span>
         <?php endif ?>
     </div>
+    <?php endif ?>
 
     <a class="details" href="<?= Url::to(['project/view', 'id' => $model->id, 'slug' => $model->slug]) ?>">
         <img class="img-responsive lazy" src="<?=$bgImg?>" data-src="<?= $model->getPrimaryImageThumbnailRelativeUrl() ?>" />
