55b314259d8c1a0a23194f9cf73f5add40b81f30
Alexander Makarov <sam@rmcreative.ru>
2022-02-16 10:16:41 +0300

    Adjust card layout

--- views/project/_card.php
+++ views/project/_card.php
@@ -13,17 +13,19 @@ $bgImg = Yii::$app->request->baseUrl . '/img/project_no_image.png';
 ?>
 
 <article class="<?= $model->getStatusClass() ?>">
-    <?php if ($model->is_featured): ?>
-        <span class="featured" aria-hidden="true"></span>
-    <?php endif ?>
+    <div class="badges">
+        <?php if ($model->is_featured): ?>
+            <span class="featured" aria-hidden="true"></span>
+        <?php endif ?>
 
-    <?php if ($model->verified): ?>
-        <span class="verified" aria-hidden="true"></span>
-    <?php endif ?>
+        <?php if ($model->verified): ?>
+            <span class="verified" aria-hidden="true"></span>
+        <?php endif ?>
 
-    <?php if ($model->check_result === CheckerResult::NOT_YII): ?>
-        <span class="warning" aria-hidden="true"></span>
-    <?php endif ?>
+        <?php if ($model->check_result === CheckerResult::NOT_YII): ?>
+            <span class="warning" aria-hidden="true"></span>
+        <?php endif ?>
+    </div>
 
     <a class="details" href="<?= Url::to(['project/view', 'id' => $model->id, 'slug' => $model->slug]) ?>">
         <img class="img-responsive lazy" src="<?=$bgImg?>" data-src="<?= $model->getPrimaryImageThumbnailRelativeUrl() ?>" />
